5afdfe4122b06107265c585c397da4669f1b03c4
sagmadan <sagarmadan5@gmail.com>
2023-08-05 15:52:41 +0530

    Added login and signup pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 import Medicines from './pages/Medicines'
 
 function App() {
@@ -13,13 +15,24 @@ function App() {
         <div className="pages">
           <Routes>
             <Route 
+              path="/login" 
+              element={!user ? <Login /> : <Navigate to="/" />} 
+            />
+            <Route 
+              path="/signup" 
+              element={!user ? <Signup /> : <Navigate to="/" />} 
+            />
+            <Route 
               path="/medicines" 
-              element={ <Medicines/ >} 
+              element={user ? <Medicines/ > : <Navigate to="/login" />} 
+            />
+            <Route 
+              path="/" 
+              element={user ? <Navigate to="/medicines" /> : <Navigate to="/login" />} 
             />
           </Routes>
         </div>
       </BrowserRouter>
-      {user}
     </div>
   );
 }
