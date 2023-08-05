4f340c783611b3db41d3c73eeac471fb4adc9924
sagmadan <sagarmadan5@gmail.com>
2023-08-05 16:18:05 +0530

    Added medicine context

---
+++ src/context/MedicineContext.js
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from 'react'
+
+export const MedicineContext = createContext()
+
+export const medicineReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_MEDICINES': 
+      return {
+        medicines: action.payload
+      }
+    case 'CREATE_MEDICINES':
+      return {
+        medicines: [action.payload, ...state.medicines]
+      }
+    default:
+      return state
+  }
+}
+
+export const MedicinesContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(medicineReducer, {
+    medicines: null
+  })
+
+  return (
+    <MedicineContext.Provider value={{...state, dispatch}}>
+      { children }
+    </MedicineContext.Provider>
+  )
+}
