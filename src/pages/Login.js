d572064e480cc5ae76091b88c6d0e9b85d569225
sagmadan <sagarmadan5@gmail.com>
2023-08-06 01:19:38 +0530

    Added styles to signup page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,49 +13,51 @@ const Login = () => {
   }
 
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account
-        </h2>
-      </div>
+    <div class="min-h-screen bg-gradient-to-br from-white via-blue-200 to-pink-100">
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account
+          </h2>
+        </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" onSubmit={handleSubmit}>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
 
-          <div>
-            <label className="block text-sm font-medium leading-6 text-gray-900">Username:</label>
-            <div className="mt-2">
-              <input
-                className="block w-full rounded-md border border-gray-300 md:border-gray-100 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                type="text"
-                onChange={(e) => setUsername(e.target.value)}
-                value={username}
-              />
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">Username:</label>
+              <div className="mt-2">
+                <input
+                  className="block w-full rounded-md border border-gray-300 md:border-gray-100 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  type="text"
+                  onChange={(e) => setUsername(e.target.value)}
+                  value={username}
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <label className="block text-sm font-medium leading-6 text-gray-900">Password:</label>
-            <div className="mt-2">
-              <input
-                className="block w-full rounded-md border border-gray-300 md:border-gray-100 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">Password:</label>
+              <div className="mt-2">
+                <input
+                  className="block w-full rounded-md border border-gray-300 md:border-gray-100 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <button
-              disabled={isLoading}
-              className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              Log in
-            </button>
-            {error && <div className="error">{error}</div>}
-          </div>
+            <div>
+              <button
+                disabled={isLoading}
+                className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Log in
+              </button>
+              {error && <div className="mt-5 text-red-500">{error}</div>}
+            </div>
 
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   )
